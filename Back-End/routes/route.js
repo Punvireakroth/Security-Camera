bc4e7b33942d0e09ceef7929e1c062ee5e3db3b3
Punvireakroth <vireakrothpun@gmail.com>
2023-01-18 14:25:25 +0700

    update route

--- Back-End/routes/route.js
+++ Back-End/routes/route.js
@@ -1,9 +1,9 @@
 const router = require("express").Router();
 
-const { signup, getbill } = require("../controller/appController.js");
+const { getbill } = require("../controller/appController.js");
 
 // HTTP Request
-router.post("/user/signup", signup);
+// router.post("/user/signup", signup);
 router.post("/product/getbill", getbill);
 
 module.exports = router;
